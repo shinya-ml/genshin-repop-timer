e18d248d4dc088e75c368815c92e269817a21406
shinya-ml <epsonia_ss08@icloud.com>
2023-12-29 16:45:22 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -50,7 +50,7 @@ app.post("/", async (c) => {
         };
         return c.json({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-          data: { content: repopInfo },
+          data: { content: `registerd: ${JSON.stringify(repopInfo)}` },
         });
       }
     }
