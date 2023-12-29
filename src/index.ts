1a42393226c33358af9bf7ee5786dd1d5d4ca12b
shinya-ml <epsonia_ss08@icloud.com>
2023-12-29 16:12:32 +0900

    biome

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,45 @@
-import { Hono } from 'hono'
-import {InteractionType, InteractionResponseType, verifyKey} from 'discord-interactions'
+import { Hono } from "hono";
+import {
+  InteractionType,
+  InteractionResponseType,
+  verifyKey,
+} from "discord-interactions";
 
 type Bindings = {
-    APPLICATION_ID: string
-    PUBLIC_KEY:string
-    TOKEN: string
-}
-const app = new Hono<{Bindings: Bindings}>()
+  APPLICATION_ID: string;
+  PUBLIC_KEY: string;
+  TOKEN: string;
+};
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.post('/', async (c) => {
-    const signature = c.req.header('X-Signature-Ed25519') ?? ''
-    const timestamp = c.req.header('X-Signature-Timestamp') ?? ''
-    const raw = await c.req.raw.clone().text()
-    const isValid = verifyKey(raw, signature, timestamp, c.env.PUBLIC_KEY)
-    if (!isValid) {
-        return c.json({message: 'invalid request'}, 401)
+app.post("/", async (c) => {
+  const signature = c.req.header("X-Signature-Ed25519") ?? "";
+  const timestamp = c.req.header("X-Signature-Timestamp") ?? "";
+  const raw = await c.req.raw.clone().text();
+  const isValid = verifyKey(raw, signature, timestamp, c.env.PUBLIC_KEY);
+  if (!isValid) {
+    return c.json({ message: "invalid request" }, 401);
+  }
+  const body = await c.req.json();
+  if (body.type === InteractionType.PING) {
+    return c.json({ type: 1 }, 200);
+  }
+  if (body.type === InteractionType.APPLICATION_COMMAND) {
+    switch (body.data.name) {
+      case "test": {
+        return c.json({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: { content: "hello world" },
+        });
+      }
+      case "register": {
+        return c.json({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: { content: "registered" },
+        });
+      }
     }
-    const body = await c.req.json()
-    if (body.type === InteractionType.PING) {
-        return c.json({type: 1}, 200)
-    }
-    return c.json({type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE, data: {content: 'hello world'}})
-})
+  }
+});
 
-export default app
+export default app;
